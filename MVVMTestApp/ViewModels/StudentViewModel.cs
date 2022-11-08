52ae76c1dcca99a2c40127f823b972e0660832fc
Sanjin Gološ <sanjin.golos@edu.fit.ba>
2022-11-08 09:52:41 +0100

    commit

---
+++ MVVMTestApp/ViewModels/StudentViewModel.cs
@@ -0,0 +1,48 @@
+﻿using MVVMTestApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMTestApp.ViewModels
+{
+
+    public class StudentViewModel
+    {
+        public MyICommand DeleteCommand { get; set; }
+        public StudentViewModel()
+        {
+            LoadStudents();
+            DeleteCommand = new MyICommand(OnDelete, CanDelete);
+        }
+
+        public ObservableCollection<Student> Students
+        {
+            get;
+            set;
+        }
+
+        public void LoadStudents()
+        {
+            ObservableCollection<Student> students = new ObservableCollection<Student>();
+
+            students.Add(new Student { FirstName = "Mark", LastName = "Allain" });
+            students.Add(new Student { FirstName = "Allen", LastName = "Brown" });
+            students.Add(new Student { FirstName = "Linda", LastName = "Hamerski" });
+
+            Students = students;
+        }
+
+        private Student _selectedStudent;
+        public Student SelectedStudent { get { return _selectedStudent; } set { _selectedStudent = value; DeleteCommand.RaiseCanExecuteChanged(); } }
+
+
+        private void OnDelete() { Students.Remove(SelectedStudent); }
+        private bool CanDelete()
+        {
+            return SelectedStudent != null;
+        }
+    }
+}
